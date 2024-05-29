423bfbfad303b248efe4b8926a588f9283ea0731
munkhlei <kh.me011230@gmail.com>
2024-05-30 05:15:35 +0800

    responsive

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,22 +1,24 @@
-'use client'
-import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+"use client";
+import * as React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+import styles from "./Graph.module.css";
 
 export default function BasicLineChart() {
   return (
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-          area: true,
-          color: '#FF5733',
-        },
-        
-      ]}
-      width={550}
-      height={400}
-      grid={{horizontal: true}}
-    />
+    <div className={styles.chartContainer}>
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            area: true,
+            color: "#FF5733",
+          },
+        ]}
+        width={undefined}
+        height={400}
+        grid={{ horizontal: true }}
+      />
+    </div>
   );
 }
